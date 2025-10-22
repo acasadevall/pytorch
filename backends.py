de365b93557e8694e4463a5c047be91a95b78944
Arnau Casadevall <arnau.casadevall.saiz@gmail.com>
2025-10-22 12:54:08 +0200

    build utils + get_torch_version
    Signed-off-by: Arnau Casadevall <arnau.casadevall.saiz@gmail.com>

---
+++ backends.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import torch
+
+if __name__ == "__main__":    
+    backends = ['cpu', 'cuda', 'ipu', 'xpu', 'mkldnn', 'opengl', 'opencl', 'ideep', 'hip', 've', 'fpga', 'maia', 'xla', 'lazy', 'vulkan', 'mps', 'meta', 'hpu', 'mtia']
+    
+    for b in backends:
+        try:
+            r = torch.randn((), device=torch.device(b), dtype=torch.float)
+            print(f"OK: {b}")
+        except:
+            print(f"ERROR: {b}")
+    
---
+++ get_torch_version.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+from tools.generate_torch_version import get_torch_version
+
+if __name__ == "__main__":
+    print(get_torch_version())
